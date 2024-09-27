21b16633bc875b83d12e8ecb38f7f1a7636ae834
not-a-cow <kly3333aswd@gmail.com>
2024-09-27 14:56:33 -0700

    improve code comments and embeds

--- src/utils/embedUtils.py
+++ src/utils/embedUtils.py
@@ -5,56 +5,61 @@ from utils.jsonDataUtils import getData
 
 
 async def success_embed(
-    interaction,
-    message: str = None,
-    title: str = None,
-    fields: list = None,
-    thumbnail: str = None,
-    image: str = None,
-    channel: str = None,
-    ephemeral: bool = False,
-    view: discord.ui.View = None,
+        interaction=None,  # required
+        message: str = None,  # required
+        title: str = None,
+        fields: list = None,
+        thumbnail: str = None,
+        image: str = None,
+        channel: int = None,
+        ephemeral: bool = False,
+        view: discord.ui.View = None,
 ):
-    if title:
-        embed = discord.Embed(
-            title=title, description=message, color=discord.Color.green()
-        )
-    else:
-        embed = discord.Embed(
-            title='', description=message, color=discord.Color.green()
-        )
+    if interaction is None:
+        print("Someones stupid ass forgot to add interaction to a `color_embed` call")
+        return None
 
-    if fields:
+    if message is None:
+        await error_embed(interaction, message="Error: no message was provided for embed creation")
+
+    embed = discord.Embed(
+        title=title, description=message, color=discord.Color.green()
+    )
+
+    if fields:  # if there are any info fields then it adds them to the embed
         for field in fields:
             embed.add_field(name=field[0], value=field[1], inline=field[2])
 
-    if thumbnail:
+    # TODO validate if thumbnail is a valid URL
+    if thumbnail:  # if there is a thumbnail then it adds it to the embed
         embed.set_thumbnail(url=thumbnail)
 
-    if image:
+    # TODO validate if image is a valid URL
+    if image:  # if there is an image then it adds it to the embed
         embed.set_image(url=image)
 
+    channel_obj = None
+    if channel:  # if a channel is specified then it sends the embed to that channel
+        channel_obj = interaction.guild.get_channel(channel)
+        if channel_obj is None:
+            raise ValueError(f'Channel with ID {channel} not found')
+
+    # if a view (like buttons and dropdowns) are included then it sends the embed with the view, along with the checks for if channel is specified
     if view:
-        if channel:
-            channel_obj = interaction.guild.get_channel(channel)
-            if channel_obj is None:
-                raise ValueError(f'Channel with ID {channel} not found')
+        if channel_obj:
             await channel_obj.send(embed=embed, ephemeral=ephemeral, view=view)
         else:
             await interaction.followup.send(embed=embed, ephemeral=ephemeral, view=view)
     else:
-        if channel:
-            channel_obj = interaction.guild.get_channel(channel)
-            if channel_obj is None:
-                raise ValueError(f'Channel with ID {channel} not found')
+        if channel_obj:
             await channel_obj.send(embed=embed, ephemeral=ephemeral)
         else:
             await interaction.followup.send(embed=embed, ephemeral=ephemeral)
 
 
 async def error_embed(
-    interaction,
-    message: str = None,
+    interaction = None, # required
+    message: str = None, # required
     title: str = None,
     fields: list = None,
     thumbnail: str = None,
@@ -63,44 +68,51 @@ async def error_embed(
     ephemeral: bool = False,
     view: discord.ui.View = None,
 ):
-    if title:
-        embed = discord.Embed(
-            title=title, description=message, color=discord.Color.red()
-        )
-    else:
-        embed = discord.Embed(title='', description=message, color=discord.Color.red())
+    if interaction is None:
+        print("Someones stupid ass forgot to add interaction to a `color_embed` call")
+        return None
 
-    if fields:
+    if message is None:
+        await error_embed(interaction, message="Error: no message was provided for embed creation")
+
+    embed = discord.Embed(
+        title=title, description=message, color=discord.Color.red()
+    )
+
+    if fields:  # if there are any info fields then it adds them to the embed
         for field in fields:
             embed.add_field(name=field[0], value=field[1], inline=field[2])
 
-    if thumbnail:
+    # TODO validate if thumbnail is a valid URL
+    if thumbnail:  # if there is a thumbnail then it adds it to the embed
         embed.set_thumbnail(url=thumbnail)
 
-    if image:
+    # TODO validate if image is a valid URL
+    if image:  # if there is an image then it adds it to the embed
         embed.set_image(url=image)
 
+    channel_obj = None
+    if channel:  # if a channel is specified then it sends the embed to that channel
+        channel_obj = interaction.guild.get_channel(channel)
+        if channel_obj is None:
+            raise ValueError(f'Channel with ID {channel} not found')
+
+    # if a view (like buttons and dropdowns) are included then it sends the embed with the view, along with the checks for if channel is specified
     if view:
-        if channel:
-            channel_obj = interaction.guild.get_channel(channel)
-            if channel_obj is None:
-                raise ValueError(f'Channel with ID {channel} not found')
+        if channel_obj:
             await channel_obj.send(embed=embed, ephemeral=ephemeral, view=view)
         else:
             await interaction.followup.send(embed=embed, ephemeral=ephemeral, view=view)
     else:
-        if channel:
-            channel_obj = interaction.guild.get_channel(channel)
-            if channel_obj is None:
-                raise ValueError(f'Channel with ID {channel} not found')
+        if channel_obj:
             await channel_obj.send(embed=embed, ephemeral=ephemeral)
         else:
             await interaction.followup.send(embed=embed, ephemeral=ephemeral)
 
 
 async def color_embed(
-    interaction,
-    message: str = None,
+    interaction = None, # required
+    message: str = None, # required
     title: str = None,
     fields: list = None,
     thumbnail: str = None,
@@ -109,41 +121,51 @@ async def color_embed(
     ephemeral: bool = False,
     view: discord.ui.View = None,
 ):
-    user_id = str(interaction.user.id)
-    color = getData('src/data/userData.json', user_id, 'preferred_color')
+    if interaction is None:
+        print("Someones stupid ass forgot to add interaction to a `color_embed` call")
+        return None
+
+    if message is None:
+        await error_embed(interaction, message="Error: no message was provided for embed creation")
+
+    user_id = str(interaction.user.id) # get user id of the person who interacted
+
+    color = getData('src/data/userData.json', user_id, 'preferred_color') # gets the preferred color of the user
     if color is None:
-        color = int('36393F', 16)
+        color = int('36393F', 16) # if no preferred color is found then it defaults to like a grayish color
     else:
-        color = int(color, 16)
+        color = int(color, 16) # if a preferred color is found then it converts it from hex to the format that works for discord
 
-    if title:
-        embed = discord.Embed(title=title, description=message, color=color)
-    else:
-        embed = discord.Embed(title='', description=message, color=color)
+    embed = discord.Embed(
+        title=title, description=message, color=color
+    )
 
-    if fields:
+    if fields:  # if there are any info fields then it adds them to the embed
         for field in fields:
             embed.add_field(name=field[0], value=field[1], inline=field[2])
 
-    if thumbnail:
+    # TODO validate if thumbnail is a valid URL
+    if thumbnail:  # if there is a thumbnail then it adds it to the embed
         embed.set_thumbnail(url=thumbnail)
 
-    if image:
+    # TODO validate if image is a valid URL
+    if image:  # if there is an image then it adds it to the embed
         embed.set_image(url=image)
 
+    channel_obj = None
+    if channel:  # if a channel is specified then it sends the embed to that channel
+        channel_obj = interaction.guild.get_channel(channel)
+        if channel_obj is None:
+            raise ValueError(f'Channel with ID {channel} not found')
+
+    # if a view (like buttons and dropdowns) are included then it sends the embed with the view, along with the checks for if channel is specified
     if view:
-        if channel:
-            channel_obj = interaction.guild.get_channel(channel)
-            if channel_obj is None:
-                raise ValueError(f'Channel with ID {channel} not found')
+        if channel_obj:
             await channel_obj.send(embed=embed, ephemeral=ephemeral, view=view)
         else:
             await interaction.followup.send(embed=embed, ephemeral=ephemeral, view=view)
     else:
-        if channel:
-            channel_obj = interaction.guild.get_channel(channel)
-            if channel_obj is None:
-                raise ValueError(f'Channel with ID {channel} not found')
+        if channel_obj:
             await channel_obj.send(embed=embed, ephemeral=ephemeral)
         else:
             await interaction.followup.send(embed=embed, ephemeral=ephemeral)
@@ -156,15 +178,18 @@ fields = {
     ('title 1', 'value 1', True),
     ('title 2', 'value 2', True)
 }
+"""
 # true/false determines if the field is inline or not
 
+"""
 await success_embed(interaction,
-    title='Title goes here',                    #(optional)
-    message='Message',                          #(optional)
-    fields=fields,                              #(optional)
-    thumbnail='https://example.com/image.jpg'   #(optional)
-    image='https://example.com/image.jpg'       #(optional)
-    channel='channel_id',                       #(optional)
-    view=your_view_instance                     #(optional)
+    title='Title goes here', 
+    message='Message', 
+    fields=fields, 
+    thumbnail='https://example.com/image.jpg'
+    image='https://example.com/image.jpg'
+    channel='channel_id',
+    view=your_view_instance
     )
 """
+# all params are optional besides interaction, no clue why you would want to call this without any message, but you can
